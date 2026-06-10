7e156659a124b25de568f0579d1b590cea49a413
Jayanta Mahato <mahatojayanta69@gmail.com>
2026-06-10 19:48:39 +0530

    Updated project

--- app.js
+++ app.js
@@ -646,7 +646,26 @@
 
 // clearInterval();
 
-let id = setInterval(() =>{
-    console.log("hello");
-},2000);
-console.log(id);
+// let id = setInterval(() =>{
+//     console.log("hello");
+// },2000);
+// console.log(id);
+
+
+// let a = 20;
+// let b = 30;
+// console.log(a+b);
+
+// console.log(a-b);
+// console.log(a*b);
+
+// let num = [1,2,3,4];
+// let double = num.map((el) => {
+//     return el * 2;
+// });
+
+let nums = [1,2,3,4,5,6,7,8,9];
+let ans = nums.filter((el) => {
+    return el % 2 == 0;
+});
+console.log(ans);
